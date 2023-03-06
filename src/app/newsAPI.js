d517a54705e8380a58bb0ee44b2a89944f3551e8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-03-06 23:51:10 +0200

    add function fetch search API

---
+++ src/app/newsAPI.js
@@ -0,0 +1,8 @@
+const BASE_URL = `https://newsapi.org/v2/`;
+const NEWS_API_KEY = `1572576c777b4bada530e28e03640c11`;
+
+// SEARCH
+export function fetchSearchAPI(query) {
+  const SEARCH_URL = `${BASE_URL}everything?apiKey=${NEWS_API_KEY}&q=${query}&searchIn=title&from=2023-03-04&to=2023-03-05&language=en&sortBy=popularity&pageSize=24`;
+  return fetch(SEARCH_URL).then(response => response.json());
+}
