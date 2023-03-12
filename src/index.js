0ecf4b25b8c017f7d1e5f6c2d105a39710c51d34
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-03-12 16:06:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,66 @@
-import { format } from 'date-fns';
-import { uk } from 'date-fns/locale';
 import debounce from 'lodash.debounce';
 import { Report } from 'notiflix/build/notiflix-report-aio';
-import { fetchSearchAPI, optionsListNews, URL_LIST_NEWS } from './app/newsAPI';
-import { setImages, setTitleImages } from './app/setImages';
+import { fetchSearchAPI, URL_LIST_NEWS } from './app/newsAPI';
 import { createMarkupArticle, insertContent } from './app/createMarkup';
 
 const list = document.querySelector('[data-list]');
+const input = document.querySelector('#search-input');
 const form = document.querySelector('#form');
+const selectCountry = form.querySelector('#country');
+const selectLanguage = form.querySelector('#language');
+const selectCategory = form.querySelector('#category');
 
-// add debounce if use only input without form
-// form.addEventListener('input', debounce(onSubmit), 300);
+// console.log(form, selectCountry, selectLanguage, selectCategory);
+
+input.addEventListener('input', debounce(onInputSearch), 300);
 form.addEventListener('submit', onSubmit);
+
+// !----------- createURL -----------------------------
+
+function createURL() {
+  const countries = selectCountry.value;
+  const categories = selectCategory.value;
+  const lang = selectLanguage.value;
+
+  console.log(categories, countries, lang);
+
+  const url = `https://newsdata.io/api/1/news?apikey=pub_1872016d3bdbc1e153439b60c6c670f1f5aed&country=${countries}&category=${categories}&language=${lang}`;
+  return url;
+}
+
+// !--------- CHECK CATEGORY AND SEARCH ---------------
 
 function onSubmit(e) {
   e.preventDefault();
 
-  const value = e.currentTarget.elements.news.value.trim();
-  //console.log(value);
+  const url = createURL();
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ results }) => {
+      console.log(results.length);
 
+      if (!results.length) {
+        throw new Error('Not articles found');
+      }
+
+      return results.reduce(
+        (markup, article) => markup + createMarkupArticle(article),
+        ''
+      );
+    })
+    .then(markup => {
+      list.innerHTML = markup;
+    })
+    .catch(onError)
+    .finally(() => {
+      checkSymbols(400);
+    });
+}
+
+// !------INPUT SEARCH -------------------------
+function onInputSearch(e) {
+  const value = input.value.trim();
+  //console.log(value);
   fetchSearchAPI(value)
     .then(({ results }) => {
       console.log(results.length);
@@ -35,32 +77,12 @@ function onSubmit(e) {
     })
     .catch(onError)
     .finally(() => {
-      const contentEls = document.querySelectorAll('.card__content');
-      const maxHeight =
-        parseInt(window.getComputedStyle(contentEls[0]).lineHeight) * 20;
-      contentEls.forEach(contentEl => {
-        const contentText = contentEl.innerText;
-        if (contentText.length > 400) {
-          contentEl.classList.add('long-text');
-        }
-        const lines = Math.ceil(
-          contentEl.clientHeight /
-            parseInt(window.getComputedStyle(contentEl).lineHeight)
-        );
-        if (lines > 20) {
-          const maxLength = 400;
-          const ellipsis = '...';
-          if (contentText.length > maxLength) {
-            const truncatedText = contentText
-              .substring(0, maxLength)
-              .replace(/(\.|!|\?)\s/g, '$1' + ellipsis + ' ');
-            contentEl.innerText = truncatedText + ellipsis;
-          }
-        }
-      });
-      form.reset();
+      checkSymbols(400);
+      input.innerHTML = '';
     });
 }
+
+//! --- ERROR -------------------------------------
 
 function onError(err) {
   Report.failure(
@@ -70,76 +92,45 @@ function onError(err) {
   );
 }
 
+// ! ---- CHECK SYMBOLS ---------------------------------
+
+function checkSymbols(maxNum) {
+  if (!document.querySelectorAll('.card__content')) return;
+  const contentEls = document.querySelectorAll('.card__content');
+  const maxHeight =
+    parseInt(window.getComputedStyle(contentEls[0]).lineHeight) * 20;
+  contentEls.forEach(contentEl => {
+    const contentText = contentEl.innerText;
+    if (contentText.length > maxNum) {
+      contentEl.classList.add('long-text');
+    }
+    const lines = Math.ceil(
+      contentEl.clientHeight /
+        parseInt(window.getComputedStyle(contentEl).lineHeight)
+    );
+    if (lines > 20) {
+      const maxLength = maxNum;
+      const ellipsis = '...';
+      if (contentText.length > maxLength) {
+        const truncatedText = contentText
+          .substring(0, maxLength)
+          .replace(/(\.|!|\?)\s/g, '$1' + ellipsis + ' ');
+        contentEl.innerText = truncatedText + ellipsis;
+      }
+    }
+  });
+}
+
+//! -------- main page -----------------------------------
 fetch(URL_LIST_NEWS)
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error fetching`);
+    }
+    return response.json();
+  })
   .then(({ results }) => {
-    // if (!status.ok) {
-    //   throw new Error(`Error fetching`);
-    // }
-    //console.log(results);
+    console.log(results);
     insertContent(results);
   })
-  .catch(onError)
-  .finally(() => {
-    const contentEls = document.querySelectorAll('.card__content');
-    const maxHeight =
-      parseInt(window.getComputedStyle(contentEls[0]).lineHeight) * 20; // максимальна висота контейнера (20 рядки тексту)
-    contentEls.forEach(contentEl => {
-      const contentText = contentEl.innerText;
-      if (contentText.length > 800) {
-        contentEl.classList.add('long-text'); // додає клас long-text, якщо довжина тексту перевищує 350 символів
-      }
-      const lines = Math.ceil(
-        contentEl.clientHeight /
-          parseInt(window.getComputedStyle(contentEl).lineHeight)
-      );
-      if (lines > 20) {
-        const maxLength = 800;
-        const ellipsis = '...';
-        if (contentText.length > maxLength) {
-          const truncatedText = contentText
-            .substring(0, maxLength)
-            .replace(/(\.|!|\?)\s/g, '$1' + ellipsis + ' ');
-          contentEl.innerText = truncatedText + ellipsis;
-        }
-      }
-    });
-  });
-
-// --------------NEWS API---------------------------------------
-
-// fetch(URL_LIST_NEWS, optionsListNews)
-//   .then(response => response.json())
-//   .then(({ articles }) => {
-//     // if (!ok) {
-//     //   throw new Error(`Error fetching`);
-//     // }
-//     console.log(articles);
-//     insertContent(articles);
-//   })
-//   .catch(onError);
-
-// function onSubmit(e) {
-//   e.preventDefault();
-
-//   const value = e.currentTarget.elements.news.value.trim();
-
-//   fetchSearchAPI(value)
-//     .then(({ articles }) => {
-//       if (articles.length === 0) {
-//         throw new Error('No articles');
-//       }
-//       return articles.reduce(
-//         (markup, article) => markup + createMarkupArticle(article),
-//         ''
-//       );
-//     })
-//     .then(markup => (list.innerHTML = markup))
-//     .catch(onError)
-//     .finally(() => form.reset());
-//   console.log(document.querySelector('li:nth-child(3)'));
-// }
-
-// function onError(err) {
-//   console.log(err);
-// }
+  .catch(onError);
